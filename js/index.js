6b9e8b4739890e5a90a1886d4ef3dc20d23afc87
Kevin Haxhi <kevin.haxhi@softup.co>
2022-07-19 23:05:38 +0200

    Defined and executed render functions

--- js/index.js
+++ js/index.js
@@ -48,3 +48,32 @@ const onSaveNote = function (e) {
 noteForm.addEventListener('submit', onFormSubmit);
 saveNoteButton.addEventListener('click', onSaveNote);
 clearFormButton.addEventListener('click', onClearNote);
+
+// Render functions
+
+const renderNoteCard = function (noteData) {
+  const noteCard = document.createElement('div');
+  const noteHeader = document.createElement('h3');
+  const noteDescription = document.createElement('p');
+
+  noteCard.classList.add('note-card');
+  noteHeader.classList.add('header');
+  noteHeader.textContent = noteData.title;
+  noteDescription.classList.add('description');
+  noteDescription.textContent = noteData.description;  
+  noteCard.append(noteHeader, noteDescription);
+
+  return noteCard;
+};
+
+const renderNotes = function () {
+  const notes = arrayOfNotes.map(function (note) {
+    return renderNoteCard(note);
+  });
+
+  notesSection.append(...notes);
+}
+
+// Execution
+
+renderNotes(arrayOfNotes);
