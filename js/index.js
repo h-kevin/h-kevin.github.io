77042cfdeacd937502c6ad03d1b6c6383cb59523
Kevin Haxhi <kevin.haxhi@softup.co>
2022-07-19 23:03:39 +0200

    Set global variables and stored target elements

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Global variables
+
+const arrayOfNotes = [];
+
+// Get the target elements
+
+const notesSection = document.querySelector('.notes-section');
+const noteForm = document.querySelector('.note-form');
+const clearFormButton = document.querySelector('.clear-form');
+const saveNoteButton = document.querySelector('.save-note');
+const titleInput = document.querySelector('.form-group .title');
+const descriptionInput = document.querySelector('.form-group .description');
