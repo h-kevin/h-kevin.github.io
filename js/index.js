67c83361b74a0aa8bf0151cb2b8049a7d4f27a0e
Kevin Haxhi <kevin.haxhi@softup.co>
2022-07-19 23:04:02 +0200

    Implemented event handlers

--- js/index.js
+++ js/index.js
@@ -10,3 +10,35 @@ const clearFormButton = document.querySelector('.clear-form');
 const saveNoteButton = document.querySelector('.save-note');
 const titleInput = document.querySelector('.form-group .title');
 const descriptionInput = document.querySelector('.form-group .description');
+
+// Event handlers
+
+const onFormSubmit = function (e) {
+  e.preventDefault();
+};
+
+const onClearNote = function (e) {
+  e.preventDefault();
+
+  titleInput.value = '';
+  descriptionInput.value = '';
+}
+
+const onSaveNote = function (e) {
+  e.preventDefault();
+
+  const title = titleInput.value;
+  const description = descriptionInput.value;
+
+  if (title === '' || description === '') {
+    return;
+  } else {
+    arrayOfNotes.push({
+      title,
+      description,
+    });
+  }
+
+  onClearNote(e);
+  renderNotes(arrayOfNotes);
+}
